6b090e1a8a00dffb6c1f076d4f804b9a5c3a6629
Manjunath Davanam <manjunathd@ilimi.in>
2020-02-11 15:16:47 +0530

    Issue SB-17458 fix: Assessment report job fixes

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/DatasetUtil.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/DatasetUtil.scala
@@ -1,39 +1,37 @@
 package org.ekstep.analytics.framework.util
 
-import org.apache.spark.sql.Dataset
-import org.apache.spark.sql.Row
-import org.ekstep.analytics.framework.dispatcher.S3Dispatcher
-import org.ekstep.analytics.framework.StorageConfig
-import org.ekstep.analytics.framework.exception.DispatcherException
-import org.apache.spark.sql.functions.col
 import java.nio.file.Paths
+
+import org.apache.spark.sql.{Dataset, Row}
+import org.apache.spark.sql.functions.col
+import org.ekstep.analytics.framework.StorageConfig
 
 class DatasetExt(df: Dataset[Row]) {
 
   private val fileUtil = new HadoopFileUtil();
-  
+
   private def getTempDir(filePrefix: String, reportId: String): String = {
     Paths.get(filePrefix, reportId, "/_tmp/").toString()
   }
-  
+
   private def getFinalDir(filePrefix: String, reportId: String): String = {
     Paths.get(filePrefix, reportId).toString();
   }
-  
+
   private def filePaths(dims: Seq[String], row: Row, format: String, tempDir: String, finalDir: String): (String, String) = {
-    
+
     val dimPaths = for(dim <- dims) yield {
       dim + "=" + row.get(row.fieldIndex(dim))
     }
-    
+
     val paths = for(dim <- dims) yield {
       row.get(row.fieldIndex(dim))
     }
-    
+
     (Paths.get(tempDir, dimPaths.mkString("/")).toString(), Paths.get(finalDir, paths.mkString("/")) + "." + format)
   }
 
-  def saveToBlobStore(storageConfig: StorageConfig, format: String, reportId: String, options: Option[Map[String, String]], partitioningColumns: Option[Seq[String]]) = {
+  def saveToBlobStore(storageConfig: StorageConfig, format: String, reportId: String, options: Option[Map[String, String]], partitioningColumns: Option[Seq[String]]):String = {
 
     val conf = df.sparkSession.sparkContext.hadoopConfiguration;
 
@@ -45,7 +43,7 @@ class DatasetExt(df: Dataset[Row]) {
       case _ =>
         storageConfig.fileName
     }
-    
+
     val filePrefix = storageConfig.store.toLowerCase() match {
       case "s3" =>
         "s3n://"
@@ -54,7 +52,7 @@ class DatasetExt(df: Dataset[Row]) {
       case _ =>
         ""
     }
-    
+
     val tempDir = getTempDir(file, reportId);
     val finalDir = getFinalDir(file, reportId);
 
@@ -72,15 +70,15 @@ class DatasetExt(df: Dataset[Row]) {
     } else {
       df.write.format(format).options(opts).save(filePrefix + tempDir);
       fileUtil.delete(conf, filePrefix + finalDir + "." + format)
-      fileUtil.copyMerge(filePrefix + tempDir, filePrefix + finalDir + "." + format, conf, true);  
+      fileUtil.copyMerge(filePrefix + tempDir, filePrefix + finalDir + "." + format, conf, true);
     }
     fileUtil.delete(conf, filePrefix + tempDir)
-    
+    file
   }
-  
+
 }
 
 object DatasetUtil {
   implicit def extensions(df: Dataset[Row]) = new DatasetExt(df);
-  
+
 }
