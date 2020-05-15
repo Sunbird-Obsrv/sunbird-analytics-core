288e21c7f8efe1228491d5ca38466c97a7caf60b
SowmyaDixit <sowmyadixit7@gmail.com>
2020-05-15 16:18:53 +0530

    Issue #0000 fix: Kafka config changes

--- analytics-core/src/main/scala/org/ekstep/analytics/streaming/KafkaEventProducer.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/streaming/KafkaEventProducer.scala
@@ -28,11 +28,13 @@ object KafkaEventProducer {
 
         // Zookeeper connection properties
         val props = new HashMap[String, Object]()
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 100.asInstanceOf[Integer]);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 10000.asInstanceOf[Integer]);
         props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 60000.asInstanceOf[Integer]);
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy")
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 10.asInstanceOf[Integer])
 
         new KafkaProducer[String, String](props);
     }
