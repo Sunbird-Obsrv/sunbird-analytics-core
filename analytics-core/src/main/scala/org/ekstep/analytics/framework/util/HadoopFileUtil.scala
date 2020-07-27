10dcd8ad11c5228eaf853b66ce197a9c5ee18474
RevathiKotla <revathi21kotla@gmail.com>
2020-07-27 10:20:06 +0530

    Issue #TG-360- Fix the merge file issue with insightcluster spark version

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/HadoopFileUtil.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/HadoopFileUtil.scala
@@ -1,42 +1,66 @@
 package org.ekstep.analytics.framework.util
 
+import java.io.IOException
+
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.Path
-import org.apache.hadoop.fs.FileUtil
+import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
+import org.apache.hadoop.io.IOUtils
+
+import scala.util.Try
 
 class HadoopFileUtil {
-  
-  /**
-   * Delete a single file.
-   */
-  def delete(file: String, conf: Configuration) : Boolean = {
-    
-    val path = new Path(file);
-    val fileSystem = path.getFileSystem(conf);
-    fileSystem.delete(path, true);
-  }
-  
-  /**
-   * Delete multiple files. Different file sources (aws, azure etc) can be passed here
-   */
-  def delete(conf: Configuration, files: String*) : Seq[Boolean] = {
-    
-    for(file <- files) yield {
-      val path = new Path(file);
-      path.getFileSystem(conf).delete(path, true);
+
+    /**
+      * Delete a single file.
+      */
+    def delete(file: String, conf: Configuration): Boolean = {
+
+        val path = new Path(file);
+        val fileSystem = path.getFileSystem(conf);
+        fileSystem.delete(path, true);
     }
-    
-  }
-  
-  /**
-   * Merge a hadoop source folder/file into another file
-   */
-  def copyMerge(srcPath: String, destPath: String, conf: Configuration, deleteSrc: Boolean) {
-    
-    val srcFilePath = new Path(srcPath);
-    val destFilePath = new Path(destPath);
-    
-    FileUtil.copyMerge(srcFilePath.getFileSystem(conf), srcFilePath, destFilePath.getFileSystem(conf), destFilePath, deleteSrc, conf, null)
-  }
-  
+
+    /**
+      * Delete multiple files. Different file sources (aws, azure etc) can be passed here
+      */
+    def delete(conf: Configuration, files: String*): Seq[Boolean] = {
+
+        for (file <- files) yield {
+            val path = new Path(file);
+            path.getFileSystem(conf).delete(path, true);
+        }
+
+    }
+
+    /**
+      * Merge a hadoop source folder/file into another file
+      */
+    def copyMerge(srcPath: String, destPath: String, conf: Configuration, deleteSrc: Boolean) {
+
+        val srcFilePath = new Path(srcPath);
+        val destFilePath = new Path(destPath);
+        copyMerge(srcFilePath.getFileSystem(conf), srcFilePath, destFilePath.getFileSystem(conf), destFilePath, deleteSrc, conf)
+    }
+
+    def copyMerge(srcFS: FileSystem, srcDir: Path, dstFS: FileSystem, dstFile: Path,
+                  deleteSource: Boolean, conf: Configuration): Boolean = {
+
+        if (dstFS.exists(dstFile))
+            throw new IOException(s"Target $dstFile already exists")
+
+        if (srcFS.getFileStatus(srcDir).isDirectory) {
+            val outputFile = dstFS.create(dstFile)
+            Try {
+                srcFS.listStatus(srcDir).sortBy(_.getPath.getName)
+                  .collect {
+                      case status if status.isFile() =>
+                          val inputFile = srcFS.open(status.getPath())
+                          Try(IOUtils.copyBytes(inputFile, outputFile, conf, false))
+                          inputFile.close()
+                  }
+            }
+            outputFile.close()
+            if (deleteSource) srcFS.delete(srcDir, true) else true
+        } else false
+    }
 }
