f9380adc52d888eb0b5ca3642701a1e9830df0f7
RevathiKotla <revathi21kotla@gmail.com>
2020-11-06 13:29:12 +0530

    Issue #TG:488- Add Test case

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/MergeUtil.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/MergeUtil.scala
@@ -3,68 +3,91 @@ package org.ekstep.analytics.framework.util
 import org.apache.commons.io.FilenameUtils
 import org.apache.hadoop.fs.Path
 import org.apache.spark.SparkContext
+import org.apache.spark.sql.functions.{col, unix_timestamp, _}
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.ekstep.analytics.framework.{FrameworkContext, MergeScriptConfig, StorageConfig}
-
+import org.joda.time
+import org.joda.time.format.DateTimeFormat
+import org.ekstep.analytics.framework.util.DatasetUtil.extensions
 
 class MergeUtil {
 
-    def mergeFile(mergeConfig: MergeScriptConfig)(implicit sc: SparkContext,fc:FrameworkContext): Unit = {
+    def mergeFile(mergeConfig: MergeScriptConfig)(implicit sc: SparkContext, fc: FrameworkContext): Unit = {
         implicit val sqlContext = new SQLContext(sc)
-        import org.ekstep.analytics.framework.util.DatasetUtil.extensions
         mergeConfig.merge.files.foreach(filePaths => {
-            val mergeDF = mergeConfig.`type`.toLowerCase() match {
+            val path = new Path(filePaths("reportPath"))
+            val mergeResult:(DataFrame, StorageConfig) = mergeConfig.`type`.toLowerCase() match {
                 case "local" =>
                     val deltaDF = sqlContext.read.options(Map("header" -> "true")).csv(filePaths("deltaPath"))
                     val reportDF = sqlContext.read.options(Map("header" -> "true")).csv(filePaths("reportPath"))
-                    reportMerge(deltaDF, reportDF, mergeConfig)
+                    (mergeReport(deltaDF, reportDF, mergeConfig),StorageConfig(mergeConfig.`type`, null, FilenameUtils.getFullPathNoEndSeparator(filePaths("reportPath"))))
                 case "azure" =>
                     val deltaDF = downloadAzureFile(filePaths("deltaPath"),
                         mergeConfig.deltaFileAccess.getOrElse(true), mergeConfig.container)
 
                     val reportDF = downloadAzureFile(filePaths("reportPath"), mergeConfig.reportFileAccess.getOrElse(true),
                         mergeConfig.postContainer.getOrElse("reports"))
-                    reportMerge(deltaDF, reportDF, mergeConfig)
+                    (mergeReport(deltaDF, reportDF, mergeConfig),StorageConfig(mergeConfig.`type`, mergeConfig.postContainer.get, path.getParent.getName))
                 case _ =>
                     throw new Exception("Merge type unknown");
             }
-            mergeDF.show(false)
-            val path = new Path(filePaths("reportPath"))
-            val storageConfig =
-                mergeConfig.`type`.toLowerCase() match {
-                    case "local" =>
-                        StorageConfig(mergeConfig.`type`, null, FilenameUtils.getFullPathNoEndSeparator(filePaths("reportPath")))
-                    case "azure" =>
-                        StorageConfig(mergeConfig.`type`, mergeConfig.postContainer.get, path.getParent.getName)
-                    case _ =>
-                        throw new Exception("Merge type unknown");
-                }
-            mergeDF.saveToBlobStore(storageConfig, "csv", FilenameUtils.removeExtension(path.getName), Option(Map("header" -> "true", "mode" -> "overwrite")), None)
-            mergeDF.saveToBlobStore(storageConfig, "json", FilenameUtils.removeExtension(path.getName), Option(Map("header" -> "true", "mode" -> "overwrite")), None)
-            })
+            val mergeDF= mergeResult._1
+            mergeDF.saveToBlobStore(mergeResult._2, "csv", FilenameUtils.removeExtension(path.getName), Option(Map("header" -> "true", "mode" -> "overwrite")), None)
+            mergeDF.saveToBlobStore(mergeResult._2, "json", FilenameUtils.removeExtension(path.getName), Option(Map("header" -> "true", "mode" -> "overwrite")), None)
+        })
     }
 
 
-
-    def reportMerge(delta: DataFrame, reportDF: DataFrame, mergeConfig: MergeScriptConfig): DataFrame =
-    {
+    def mergeReport(delta: DataFrame, reportDF: DataFrame, mergeConfig: MergeScriptConfig): DataFrame = {
 
         val reportDfColumns = reportDF.columns
-        import org.apache.spark.sql.functions._
-
-        val deltaDF =if(mergeConfig.rollup > 0 && reportDF.columns.contains(mergeConfig.rollupCol.get))
+        val deltaDF = if (mergeConfig.rollup > 0 && reportDF.columns.contains(mergeConfig.rollupCol.get))
             delta.withColumn("Date", date_format(col("Date"), "dd-MM-yyyy"))
         else
             delta
         val filteredDf = reportDF.as("report").join(deltaDF.as("delta"),
-            col("report."+ mergeConfig.rollupCol.get) === col("delta."+mergeConfig.rollupCol.get), "inner")
+            col("report." + mergeConfig.rollupCol.get) === col("delta." + mergeConfig.rollupCol.get), "inner")
           .select("report.*")
 
         val finalDf = reportDF.except(filteredDf).union(deltaDF.dropDuplicates()
           .drop(deltaDF.columns.filter(p => !reportDfColumns.contains(p)): _*)
-          .select(reportDfColumns.head, reportDfColumns.tail: _*)).orderBy(unix_timestamp(col("Date"), "dd-MM-yyyy"))
-        finalDf
+          .select(reportDfColumns.head, reportDfColumns.tail: _*))
+        rollupReport(finalDf, mergeConfig).orderBy(unix_timestamp(col("Date"), "dd-MM-yyyy"))
+    }
 
+    def rollupReport(reportDF: DataFrame, mergeScriptConfig: MergeScriptConfig): DataFrame = {
+        val subtract = (x: Int, y: Int) => x - y
+        val rollupRange = subtract(mergeScriptConfig.rollupRange.get,1)
+        val maxDate = reportDF.agg(max(unix_timestamp(col("Date"), "dd-MM-yyyy")) as "Max").collect().apply(0).getAs[Long]("Max")
+        val convert = (x: Long) => x * 1000L
+        val endDate = new time.DateTime(convert(maxDate))
+        var endYear = endDate.year().get()
+        var endMonth = endDate.monthOfYear().get()
+        val startDate = mergeScriptConfig.rollupAge.get match {
+            case "ACADEMIC_YEAR" =>
+                if (endMonth <= 5)
+                    endYear = subtract(subtract(endYear, 1), rollupRange)
+                else
+                    endYear = subtract(endYear, rollupRange)
+                new time.DateTime(endYear, 6, 1, 0, 0, 0)
+            case "GEN_YEAR" =>
+                endYear = subtract(endYear, rollupRange)
+                new time.DateTime(endYear, 1, 1, 0, 0, 0)
+            case "MONTH" =>
+                endMonth = subtract(endMonth, rollupRange)
+                endYear = if (endMonth < 1) endYear + ((if (endMonth != 0) endMonth else -1) / 12).floor.toInt else endYear
+                endMonth = if (endMonth < 1) (endMonth + 12) else endMonth
+                new time.DateTime(endYear, endMonth, 1, 0, 0, 0)
+            case "WEEK" =>
+                endDate.withDayOfWeek(1).minusWeeks(rollupRange)
+            case "DAY" =>
+                endDate.minusDays(rollupRange.toInt)
+            case _ =>
+                new time.DateTime(1970, 1, 1, 0, 0, 0)
+        }
+        reportDF.filter(p => DateTimeFormat.forPattern("dd-MM-yyyy")
+          .parseDateTime(p.getAs[String]("Date"))
+          .getMillis >= startDate.asInstanceOf[time.DateTime].getMillis)
     }
 
     def downloadAzureFile(filePath: String, isPrivate: Boolean, container: String)(implicit sqlContext: SQLContext, fc: FrameworkContext): DataFrame = {
@@ -76,7 +99,7 @@ class MergeUtil {
                 fc.getStorageService("azure", "report_storage_key", "report_storage_secret")
         val keys = storageService.searchObjects(container, filePath)
 
-        sqlContext.read.options(Map("header"->"true")).csv(storageService.getPaths(container, keys).toArray.mkString(","))
+        sqlContext.read.options(Map("header" -> "true")).csv(storageService.getPaths(container, keys).toArray.mkString(","))
     }
 
 }
--- analytics-core/src/test/scala/org/ekstep/analytics/framework/util/TestMergeUtil.scala
+++ analytics-core/src/test/scala/org/ekstep/analytics/framework/util/TestMergeUtil.scala
@@ -1,6 +1,7 @@
 package org.ekstep.analytics.framework.util
 
 import org.apache.hadoop.fs.azure.AzureException
+import org.apache.spark.sql.SQLContext
 import org.ekstep.analytics.framework._
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.Matchers
@@ -28,8 +29,6 @@ class TestMergeUtil extends SparkSpec with Matchers with MockFactory {
     "MergeUtil" should "test the azure merge function" in {
 
         implicit val mockFc = mock[FrameworkContext]
-
-
         val mockStorageService = mock[BaseStorageService]
         val mergeUtil = new MergeUtil()
 
@@ -49,4 +48,63 @@ class TestMergeUtil extends SparkSpec with Matchers with MockFactory {
             mergeUtil.mergeFile(JSONUtils.deserialize[MergeScriptConfig](config))
         }
     }
+
+
+    "MergeUtil" should "test the exception case" in {
+
+        implicit val mockFc = mock[FrameworkContext]
+        val mergeUtil = new MergeUtil()
+
+        val config =
+            """{"type":"blob","id":"daily_metrics.csv","frequency":"DAY","basePath":"/mount/data/analytics/tmp","rollup":1,"rollupAge":"ACADEMIC_YEAR",
+              |"rollupCol":"Date","rollupRange":1,"merge":{"files":[{"reportPath":"apekx/daily_metrics.csv",
+              |"deltaPath":"druid-reports/ETB-Consumption-Daily-Reports/apekx/2020-11-03.csv"}],"dims":["Date"]},"container":"reports",
+              |"postContainer":"test-container","deltaFileAccess":true,"reportFileAccess":true}""".stripMargin
+
+        a[Exception] should be thrownBy {
+            mergeUtil.mergeFile(JSONUtils.deserialize[MergeScriptConfig](config))
+        }
+    }
+
+        "MergeUtil" should "test all rollup conditions" in {
+
+            implicit val mockFc = mock[FrameworkContext]
+            val mergeUtil = new MergeUtil()
+
+            val config =
+                """{"type":"local","id":"consumption_usage_metrics","frequency":"DAY","basePath":"","rollup":1,"rollupAge":"ACADEMIC_YEAR",
+                  |"rollupCol":"Date","rollupRange":2,"merge":{"files":[{"reportPath":"src/test/resources/report.csv",
+                  |"deltaPath":"src/test/resources/delta.csv"}],
+                  |"dims":["Date"]},"container":"test-container","postContainer":null,"deltaFileAccess":true,"reportFileAccess":true}""".stripMargin
+            implicit val sqlContext = new SQLContext(sc)
+            val deltaDF = sqlContext.read.options(Map("header" -> "true")).csv("src/test/resources/delta_rollup.csv")
+            val reportDF = sqlContext.read.options(Map("header" -> "true")).csv("src/test/resources/report_rollup.csv")
+            mergeUtil.mergeReport(deltaDF,reportDF,JSONUtils.deserialize[MergeScriptConfig](config)).count should be(10)
+            val config1 =
+                """{"type":"local","id":"consumption_usage_metrics","frequency":"DAY","basePath":"","rollup":1,"rollupAge":"GEN_YEAR",
+                  |"rollupCol":"Date","rollupRange":1,"merge":{"files":[{"reportPath":"src/test/resources/report.csv",
+                  |"deltaPath":"src/test/resources/delta.csv"}],
+                  |"dims":["Date"]},"container":"test-container","postContainer":null,"deltaFileAccess":true,"reportFileAccess":true}""".stripMargin
+            mergeUtil.mergeReport(deltaDF,reportDF,JSONUtils.deserialize[MergeScriptConfig](config1)).count should be(9)
+            val config2 =
+                """{"type":"local","id":"consumption_usage_metrics","frequency":"DAY","basePath":"","rollup":1,"rollupAge":"MONTH",
+                  |"rollupCol":"Date","rollupRange":1,"merge":{"files":[{"reportPath":"src/test/resources/report.csv",
+                  |"deltaPath":"src/test/resources/delta.csv"}],
+                  |"dims":["Date"]},"container":"test-container","postContainer":null,"deltaFileAccess":true,"reportFileAccess":true}""".stripMargin
+            mergeUtil.mergeReport(deltaDF,reportDF,JSONUtils.deserialize[MergeScriptConfig](config2)).count should be(8)
+            val config3 =
+                """{"type":"local","id":"consumption_usage_metrics","frequency":"DAY","basePath":"","rollup":1,"rollupAge":"WEEK",
+                  |"rollupCol":"Date","rollupRange":1,"merge":{"files":[{"reportPath":"src/test/resources/report.csv",
+                  |"deltaPath":"src/test/resources/delta.csv"}],
+                  |"dims":["Date"]},"container":"test-container","postContainer":null,"deltaFileAccess":true,"reportFileAccess":true}""".stripMargin
+            mergeUtil.mergeReport(deltaDF,reportDF,JSONUtils.deserialize[MergeScriptConfig](config3)).count should be(7)
+
+            val config4 =
+                """{"type":"local","id":"consumption_usage_metrics","frequency":"DAY","basePath":"","rollup":1,"rollupAge":"DAY",
+                  |"rollupCol":"Date","rollupRange":4,"merge":{"files":[{"reportPath":"src/test/resources/report.csv",
+                  |"deltaPath":"src/test/resources/delta.csv"}],
+                  |"dims":["Date"]},"container":"test-container","postContainer":null,"deltaFileAccess":true,"reportFileAccess":true}""".stripMargin
+            mergeUtil.mergeReport(deltaDF,reportDF,JSONUtils.deserialize[MergeScriptConfig](config4)).count should be(4)
+    }
+
 }
