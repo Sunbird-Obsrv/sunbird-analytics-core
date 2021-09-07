9bec27409e568b4f5a847522bc299a940d5c70cd
Manjunath Davanam <manjunath@sanketika.in>
2021-09-07 11:09:09 +0530

    Issue SB-24793 feat: review comments update

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CommonUtil.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CommonUtil.scala
@@ -100,7 +100,7 @@ object CommonUtil {
   def getSparkSession(parallelization: Int, appName: String, sparkCassandraConnectionHost: Option[AnyRef] = None,
                       sparkElasticsearchConnectionHost: Option[AnyRef] = None, readConsistencyLevel: Option[String] = None,
                       sparkRedisConnectionHost: Option[AnyRef] = None, sparkRedisDB: Option[AnyRef] = None,
-                      sparkRedisPort: Option[AnyRef] = Option("6379"), writeConsistencyLevel: Option[String] = None): SparkSession = {
+                      sparkRedisPort: Option[AnyRef] = Option("6379"), writeConsistencyLevel: String = "QUORUM"): SparkSession = {
     JobLogger.log("Initializing SparkSession")
     val conf = new SparkConf().setAppName(appName).set("spark.default.parallelism", parallelization.toString)
       .set("spark.driver.memory", AppConf.getConfig("spark.driver_memory"))
@@ -125,9 +125,7 @@ object CommonUtil {
       if (readConsistencyLevel.nonEmpty) {
         conf.set("spark.cassandra.input.consistency.level", readConsistencyLevel.get)
       }
-      if (writeConsistencyLevel.nonEmpty) {
-        conf.set("spark.cassandra.output.consistency.level", writeConsistencyLevel.get)
-      }
+      conf.set("spark.cassandra.output.consistency.level", Option(writeConsistencyLevel).getOrElse("QUORUM"))
       println("setting spark.cassandra.connection.host to lp-cassandra", conf.get("spark.cassandra.connection.host"))
     }
 
