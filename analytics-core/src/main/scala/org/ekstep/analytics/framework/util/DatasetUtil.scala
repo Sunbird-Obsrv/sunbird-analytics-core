1734caf4edc06884cbe1683df00a6a7593e1346e
Deepak Devadathan <deepak.devadathan@oracle.com>
2023-04-17 15:33:59 +1000

    updated dataset to include oci
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/DatasetUtil.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/DatasetUtil.scala
@@ -49,6 +49,8 @@ class DatasetExt(df: Dataset[Row]) {
         CommonUtil.getAzureFileWithoutPrefix(storageConfig.container, storageConfig.fileName, storageConfig.accountKey.getOrElse("azure_storage_key"))
       case "gcloud" =>
         CommonUtil.getGCloudFileWithoutPrefix(storageConfig.container, storageConfig.fileName);
+      case "oci" =>
+        CommonUtil.getS3FileWithoutPrefix(storageConfig.container, storageConfig.fileName);
       case _ =>
         storageConfig.fileName
     }
