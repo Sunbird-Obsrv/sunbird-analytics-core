8d260d87fefaba75cf09cd5d9a2a25daad3ed298
Manjunath Davanam <manjunath@sanketika.in>
2023-07-24 14:53:18 +0530

    #000 fix: Obsrv CSP Generalisation

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CSPUtils.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CSPUtils.scala
@@ -8,7 +8,7 @@ trait ICloudStorageProvider {
 
 object CloudStorageProviders {
   implicit val className: String = "org.ekstep.analytics.framework.util.CloudStorageProvider"
-  private val providerMap: Map[String, Class[_ <: ICloudStorageProvider]] = Map("S3" -> classOf[S3Provider], "azure" -> classOf[AzureProvider], "gcp" -> classOf[GcpProvider])
+  private val providerMap: Map[String, Class[_ <: ICloudStorageProvider]] = Map("s3" -> classOf[S3Provider], "azure" -> classOf[AzureProvider], "gcp" -> classOf[GcpProvider])
   def setSparkCSPConfigurations(sc: SparkContext, csp: String): Unit = {
     providerMap.get(csp.toLowerCase()).foreach { providerClass =>
       val providerConstructor = providerClass.getDeclaredConstructor()
@@ -18,8 +18,8 @@ object CloudStorageProviders {
   }
 }
 class S3Provider extends ICloudStorageProvider {
+  implicit val className: String = "org.ekstep.analytics.framework.util.S3Provider"
   override def setConf(sc: SparkContext): Unit = {
-    implicit val className: String = "org.ekstep.analytics.framework.util.S3Provider"
     JobLogger.log("Configuring S3 AccessKey& SecrateKey to SparkContext")
     sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", AppConf.getAwsKey())
     sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", AppConf.getAwsSecret())
@@ -31,7 +31,9 @@ class S3Provider extends ICloudStorageProvider {
 }
 
 class AzureProvider extends ICloudStorageProvider {
+  implicit val className: String = "org.ekstep.analytics.framework.util.AzureProvider"
   override def setConf(sc: SparkContext): Unit = {
+    JobLogger.log("Configuring Azure AccessKey& SecrateKey to SparkContext")
     val accName = AppConf.getStorageKey("azure")
     val accKey = AppConf.getStorageSecret("azure")
     sc.hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
@@ -40,7 +42,9 @@ class AzureProvider extends ICloudStorageProvider {
   }
 }
 class GcpProvider extends ICloudStorageProvider {
+  implicit val className: String = "org.ekstep.analytics.framework.util.GcpProvider"
   override def setConf(sc: SparkContext): Unit = {
+    JobLogger.log("Configuring GCP AccessKey& SecrateKey to SparkContext")
     sc.hadoopConfiguration.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
     sc.hadoopConfiguration.set("fs.AbstractFileSystem.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS")
     sc.hadoopConfiguration.set("fs.gs.auth.service.account.email", AppConf.getStorageKey("gcloud"))
