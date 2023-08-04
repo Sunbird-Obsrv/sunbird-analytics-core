6d55d64b4a86de48c5008f071c6933f4173f386c
Manjunath Davanam <manjunath@sanketika.in>
2023-08-04 14:16:56 +0530

    OB-525 feat: Updated the OCI Configurations

--- analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CSPUtils.scala
+++ analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CSPUtils.scala
@@ -8,7 +8,7 @@ trait ICloudStorageProvider {
 
 object CloudStorageProviders {
   implicit val className: String = "org.ekstep.analytics.framework.util.CloudStorageProvider"
-  private val providerMap: Map[String, Class[_ <: ICloudStorageProvider]] = Map("s3" -> classOf[S3Provider], "azure" -> classOf[AzureProvider], "gcp" -> classOf[GcpProvider])
+  private val providerMap: Map[String, Class[_ <: ICloudStorageProvider]] = Map("s3" -> classOf[S3Provider], "azure" -> classOf[AzureProvider], "gcp" -> classOf[GcpProvider], "oci" -> classOf[OCIProvider])
   def setSparkCSPConfigurations(sc: SparkContext, csp: String, storageKey: Option[String], storageSecret: Option[String]): Unit = {
     providerMap.get(csp.toLowerCase()).foreach { providerClass =>
       val providerConstructor = providerClass.getDeclaredConstructor()
@@ -16,11 +16,13 @@ object CloudStorageProviders {
       providerInstance.setConf(sc, storageKey, storageSecret)
     }
   }
+
+
 }
 class S3Provider extends ICloudStorageProvider {
   implicit val className: String = "org.ekstep.analytics.framework.util.S3Provider"
   override def setConf(sc: SparkContext, storageKey: Option[String], storageSecret: Option[String]): Unit = {
-    JobLogger.log("Configuring S3 AccessKey& SecrateKey to SparkContext")
+    JobLogger.log("Configuring S3 Access Key & Secret Key to SparkContext")
     val key = storageKey.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getAwsKey())
     val secret = storageSecret.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getAwsSecret())
     sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", key)
@@ -35,7 +37,7 @@ class S3Provider extends ICloudStorageProvider {
 class AzureProvider extends ICloudStorageProvider {
   implicit val className: String = "org.ekstep.analytics.framework.util.AzureProvider"
   override def setConf(sc: SparkContext, storageKey: Option[String], storageSecret: Option[String]): Unit = {
-    JobLogger.log("Configuring Azure AccessKey& SecrateKey to SparkContext")
+    JobLogger.log("Configuring Azure Access Key & Secret Key to SparkContext")
     val key = storageKey.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getStorageKey("azure"))
     val secret = storageSecret.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getStorageSecret("azure"))
     sc.hadoopConfiguration.set("fs.azure", "org.apache.hadoop.fs.azure.NativeAzureFileSystem")
@@ -46,7 +48,7 @@ class AzureProvider extends ICloudStorageProvider {
 class GcpProvider extends ICloudStorageProvider {
   implicit val className: String = "org.ekstep.analytics.framework.util.GcpProvider"
   override def setConf(sc: SparkContext, storageKey: Option[String], storageSecret: Option[String]): Unit = {
-    JobLogger.log("Configuring GCP AccessKey& SecretKey to SparkContext")
+    JobLogger.log("Configuring GCP Access Key & Secret Key to SparkContext")
     val key = storageKey.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getStorageKey("gcloud"))
     val secret = storageSecret.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getStorageSecret("gcloud"))
     sc.hadoopConfiguration.set("fs.gs.impl", "com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem")
@@ -54,5 +56,16 @@ class GcpProvider extends ICloudStorageProvider {
     sc.hadoopConfiguration.set("fs.gs.auth.service.account.email",  key)
     sc.hadoopConfiguration.set("fs.gs.auth.service.account.private.key", secret)
     sc.hadoopConfiguration.set("fs.gs.auth.service.account.private.key.id", AppConf.getConfig("gcloud_private_secret_id"))
+  }
+}
+
+class OCIProvider extends ICloudStorageProvider {
+  implicit val className: String = "org.ekstep.analytics.framework.util.OCIProvider"
+  override def setConf(sc: SparkContext, storageKey: Option[String], storageSecret: Option[String]): Unit = {
+    val key = storageKey.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getStorageKey("oci"))
+    val secret = storageSecret.filter(_.nonEmpty).map(AppConf.getConfig).getOrElse(AppConf.getStorageSecret("oci"))
+    JobLogger.log("Configuring OCI Access Key & Secret Key to SparkContext")
+    sc.hadoopConfiguration.set("fs.s3n.awsAccessKeyId", key);
+    sc.hadoopConfiguration.set("fs.s3n.awsSecretAccessKey", secret);
   }
 }
